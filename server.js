c3315e1816de5cc87205a0004ce9fb73f56e83f2
Dony Dexon <kindjaev080592@gmail.com>
2022-11-14 17:52:16 +1100

    added cors

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 require("dotenv").config()
 const express = require("express");
+const cors = require("cors") // edded 
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts.js");
 const userRoutes = require("./routes/user")
+
 
 // express app 
 const app = express();
 
 
 //========== MIDDLEWARE =========
+app.use(cors()) //added - check later what does it mean
+
 app.use(express.json()) // if any request comes in, it looks for the body and pass it to the req object
 app.use((req, res, next) => {
     console.log(req.path, req.method);
